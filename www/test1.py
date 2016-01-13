402e51940c42b6effcb96fc4ce932ffc14de7295
songkaiape <songkaiape@gmail.com>
2016-01-14 00:21:46 +0800

    add models.py

---
+++ www/test1.py
@@ -0,0 +1,16 @@
+import orm,asyncio
+from models import User, Blog, Comment
+
+def test(loop):
+
+	yield from orm.create_pool(loop=loop, user='www-data',password='www-data',db='awesome')
+
+	u =User(name= 'Test', email='test1@exapmle.com', passwd='123',image='about:blank')
+
+	yield from u.save()
+
+loop = asyncio.get_event_loop()
+
+loop.run_until_complete(test(loop))
+
+loop.close()
